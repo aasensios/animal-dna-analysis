70e92d347445baa3777c84fa8a0ac6db8f307d6a
Andres Tenesaca Burgos <andres.t.b.15@gmail.com>
2019-02-23 18:27:31 +0100

    Directives added, country model add & laboratory form finished

--- src/app/laboratory/laboratory.component.ts
+++ src/app/laboratory/laboratory.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//Model
 import { Laboratory } from '../model/laboratory';
+import { Country } from '../model/country';
 
 @Component({
   selector: 'app-laboratory',
@@ -11,12 +13,23 @@ export class LaboratoryComponent implements OnInit {
 
   //properties
   laboratory: Laboratory;
+  countries: Country[]=[];
 
   ngOnInit() {
+    let countryAux: string[] = ["Barcelona", "Badalona", "Girona"];
+
+    for (let i:number = 0; i < countryAux.length; i++) {
+        let country = new Country(i,countryAux[i]);
+
+        this.countries.push(country);
+    }
+
+    this.laboratory = new Laboratory();
+
+    this.laboratory.setCountry(this.countries[0]);
   }
 
-  laboratory(): void {
+  laboratoryInput(): void {
     console.log(this.laboratory);
   }
-
 }
